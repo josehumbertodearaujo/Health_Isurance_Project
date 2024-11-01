eaa6fe3603a5af08f4bd2776b16b59065deb4c6b
José Humberto <josehumbertodearaujo@hotmail.com>
2024-11-01 10:22:32 -0300

    update requirements

--- app_multipages/pages/1_Sobre_o_modelo.py
+++ app_multipages/pages/1_Sobre_o_modelo.py
@@ -8,3 +8,30 @@ st.set_page_config(
 )
 
 st.title('Sobre o Modelo')
+
+st.markdown(
+    """
+    ## Business Uniderstending
+    
+    O setor de seguros de saúde é altamente dinâmico e competitivo, onde a precificação adequada dos planos é fundamental para garantir 
+    tanto a sustentabilidade financeira da seguradora quanto a satisfação dos clientes. 
+    Precificar os planos de maneira precisa permite que a empresa mantenha-se competitiva no mercado e evite riscos financeiros significativos, 
+    especialmente considerando os crescentes custos médicos e as necessidades individuais dos segurados.
+
+    ### Objetivos
+    O objetivo deste projeto é desenvolver um modelo de machine learning que consiga prever com o preço do seguro saúde de um indivíduo levando em conta variáveis demográficas e 
+    médicas dos clientes.
+
+    ## Descrição dos Dados
+    Os dados inicialmente fornecidos para esse projeto são:
+    - Idade
+    - IMC
+    - Número de Filhos
+    - Status de Fumante
+    - Sexo
+    - Região
+
+    ## Análise Exploratória
+
+    """
+)
